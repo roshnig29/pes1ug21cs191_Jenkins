901406a0bc1abd3cbb299040e6a897a34ff0c30c
unknown <roshni.gopinathan@gmail.com>
2024-03-04 21:40:53 +0530

    jenkinsfile added

---
+++ main/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "SRN: PES1UG21CS191\n";
+  cout << "This is Code/script written to create basic pipeline using GitHub repository\n";
+  return 0;
+}
